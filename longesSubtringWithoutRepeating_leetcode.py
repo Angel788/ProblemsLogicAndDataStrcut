a2dad6490cf2547520b96db24a6ae24dcfb0cdf5
Angel Garcia <angel-yahel@outlook.com>
2021-11-16 14:08:41 -0600

    Add gitignore and solving problem of hash table leetcode

---
+++ longesSubtringWithoutRepeating_leetcode.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        set_str={}
+        i=0;
+        j=0;
+        max_num=0;
+        for str_s in range(len(s)):
+            char_s=s[str_s];
+            while(char_s in set_str):
+                j=max(j,set_str[char_s]+1);
+            set_str[char_s]=str_s;
+            max_num=max(max_num,str_s-j+1);
+        return max;
