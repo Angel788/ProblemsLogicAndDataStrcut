4279fd9a1a8de7a25e3870e1f9c8a623c7c8bd97
Angel Garcia <angel-yahel@outlook.com>
2022-04-08 15:11:57 -0500

    Color graph

--- New_Distributation_E65_C.cpp
+++ New_Distributation_E65_C.cpp
@@ -22,16 +22,19 @@ using namespace std;
 #define debuga(s) cout<<s<<" ";
 #define debuge cout<<endl;
 #define len(a) a.size()
-vector<int>*graph;
-bool *visited;
-int *dp;
+vector<int>graph[500002];
+bool visited[500002];
 int n;
+int color=1;
+int dp[500002]={0};
+int siz[50002];
 //void solve();
-void conectGraph(int u, v){
+void conectGraph(int u,int v){
     graph[u].pb(v);
     graph[v].pb(u);
 }
-int bfs(int v){
+void bfs(int v){
+    dp[v]=color;
     queue<int> q;
     visited[v]=true;
     q.push(v);
@@ -41,32 +44,42 @@ int bfs(int v){
         res++;
         q.pop();
         for(auto x:graph[i]){
+            if(!visited[x]){
                 q.push(x);
                 visited[x]=true;
+                dp[x]=color;
+            }
         }
     }
-    return res;
+    siz[color]=res;
 }
 
 int main(){
     fast
     int m,mi,ant,num;
     cin>>n>>m;
-    graph=new vector<int>[n+1];
-    visited=new int [n+1];
-    dp=new int[n+1];
-    dp={0};
     forn(i,m,0){
         cin>>mi;
-        cin>>num;
-        ant=num;
-        forn(i,mi,1){
-            cin>>num;
-            conectGraph(ant,num);
-            ant=num;
+        forn(i,mi,0){
+            if(i==0){
+                cin>>num;
+                conectGraph(num,num);
+                ant=num;
+            }
+            else{
+               cin>>num;
+               conectGraph(ant,num);
+               ant=num;
+
+            }
         }
     }
     forn(i,n+1,1){
-        debug(bfs(i));
+        if(dp[i]<1){
+            bfs(i);
+            color++;
+        }
+        debuga(siz[dp[i]])
     }
+    debuge
 }
