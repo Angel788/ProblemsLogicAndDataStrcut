a244b267cc81ec088727bfe0bd66abbe8290e969
Angel Garcia <angel-yahel@outlook.com>
2022-04-09 19:45:06 -0500

    Contest 02-04-2022

---
+++ 246_At_Coder_beginer/D.cpp
@@ -0,0 +1,32 @@
+//Author:Angel Garcia
+#include <bits/stdc++.h>
+using namespace std;
+#define forn(i,n,e) for(int i=e;i<n;i++)
+#define ll long long
+#define fornll(i,n,e) for(ll i=e;i<n;i++)
+#define pii pair<int,int>
+#define endl "\n";
+#define pb push_back
+#define pii pair<int,int>
+#define pis pair<int,string>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vb vector<bool>
+#define YES cout<<"YES"<<endl;
+#define Yes cout<<"Yes"<<endl;
+#define NO cout<<"NO"<<endl;
+#define No cout<<"No"<<endl;
+#define fast ios::sync_with_stdio(false);cin.tie(0);
+#define test(t) while(t--) solve();
+#define debug(s) cout<<s<<endl;
+#define debuga(s) cout<<s<<" ";
+#define debuge cout<<endl;
+#define len(a) a.size()
+#define max 1e18
+//vector<int>*graph
+// void solve();
+int mian(){
+  ll n;
+  cin>>n;
+
+}
